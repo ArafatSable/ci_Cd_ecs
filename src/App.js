2d1cc1a90d6c4b7473f469c775c9ec41bbad4475
ArafatSable <arafatsable@gmail.com>
2025-01-23 11:07:13 +0000

    Add GitHub Actions workflow for CI/CD

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Ok main code changes!!!!
+          Arafat!!!
         </a>
       </header>
     </div>
